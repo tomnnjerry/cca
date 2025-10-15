37628825f47ff267b22817b7079dfdd3b101cd34
Bishtu <bistudas.das@gmail.com>
2025-10-15 20:55:29 +0530

    only content about

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 app_name='home'
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
 ] 
--- home/views.py
+++ home/views.py
@@ -6,3 +6,11 @@ def home(request):
 
     }
     return render(request, 'index.html', context)
+
+
+def about(request):
+    context = {
+
+    }
+    return render(request, 'about.html', context)
+
